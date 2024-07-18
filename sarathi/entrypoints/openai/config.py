c909495d217a73cf77fdb50c3e7badac4eca6e72
Amey Agrawal <agrawalamey12@gmail.com>
2024-07-17 22:51:53 -0400

    Minor: Fix formatting (#26)

--- sarathi/entrypoints/openai/config.py
+++ sarathi/entrypoints/openai/config.py
@@ -7,14 +7,14 @@ from sarathi.entrypoints.config import APIServerConfig
 @dataclass
 class OpenAIServerConfig(APIServerConfig):
     api_key: Optional[str] = field(
-        default=None,
-        metadata={"help": "API key for authentication with the server."}
+        default=None, metadata={"help": "API key for authentication with the server."}
     )
     chat_template: Optional[str] = field(
-        default=None,
-        metadata={"help": "Template for formatting chat messages."}
+        default=None, metadata={"help": "Template for formatting chat messages."}
     )
     response_role: str = field(
         default="assistant",
-        metadata={"help": "Role to be assigned to the model's responses in the chat format."}
+        metadata={
+            "help": "Role to be assigned to the model's responses in the chat format."
+        },
     )
